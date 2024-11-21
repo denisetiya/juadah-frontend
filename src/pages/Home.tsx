47f4bcea373d4bdc431cf2de08051192d9e17e0c
deni setiya <denisetiya009@gmail.com>
2024-11-21 15:40:35 +0700

    🐞 fix: fix navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,15 +6,15 @@ import SwipeTestimonial from "../components/fragments/SwipeTestimonial"
 
 function Home() {
   return (
-    <div className="flex flex-col items-center w-screen h-full font-bold ">
-      <div className="bg-[#F4F5F6] h-screen w-screen px-5 2xl:px-52 flex items-center">
+    <div className="flex flex-col items-center font-bold ">
+      <div className="bg-[#F4F5F6] w-full px-5 2xl:px-52 flex items-center">
         <TitleDiscover /> 
       </div>
       <div className="mx-4 mb-10 lg:mb-0 lg:mx-0">
         <SwipePreview />
       </div>
 
-      <div>
+      <div className="">
         <PopularCake />
       </div>
 
